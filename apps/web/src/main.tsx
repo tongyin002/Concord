4cecc1d6bbcf703a950e8ec4fb90a8df426a3b20
Tong Yin <tongyin@Tongs-MacBook-Air.local>
2025-07-20 20:55:58 -0700

    replaced eslint with oxlint & set up zed

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,26 +1,27 @@
-import React from "react";
+import { Counter, Header } from "@repo/ui";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import typescriptLogo from "/typescript.svg";
-import { Header, Counter } from "@repo/ui";
 
-const App = () => (
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
-    <Header title="Web" />
-    <div className="card">
-      <Counter />
+const App = () => {
+  return (
+    <div>
+      <a href="https://vitejs.dev">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/">
+        <img
+          src={typescriptLogo}
+          className="logo vanilla"
+          alt="TypeScript logo"
+        />
+      </a>
+      <Header title="Web" />
+      <div className="card">
+        <Counter />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 createRoot(document.getElementById("app")!).render(<App />);
