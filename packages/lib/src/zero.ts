6c8523a80605bb372b520498f9481179c485f53d
Tong Yin <tongyin@Tongs-MacBook-Air.local>
2026-01-01 19:33:42 -0800

    clean up

--- packages/lib/src/zero.ts
+++ packages/lib/src/zero.ts
@@ -8,4 +8,3 @@ export { handleQueryRequest, handleMutateRequest } from '@rocicorp/zero/server';
 export { queries, type ZeroContext } from './queries';
 export { mutators } from './mutators';
 export { schema } from './zero-schema.gen';
-export { zeroDBProvider } from './db';
